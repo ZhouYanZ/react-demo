c1b098789b9691e8d19be5d35a0d8156f7a4d5d1
庸人自扰、 <elvmx47@aliyun.com>
2019-06-30 16:21:06 +0800

    用户登录email校验

--- webserver/src/views/login/index.js
+++ webserver/src/views/login/index.js
@@ -28,7 +28,7 @@ class Login extends Component {
               rules: [
                 { required: true, message: 'Please input email' },
                 {
-                  pattern: /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
+                  type: 'email',
                   message: 'Please sure email'
                 }
               ]
