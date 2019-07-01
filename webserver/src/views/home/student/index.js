0806963aac197462a0ad25eef0ab3a50a5dba9c5
庸人自扰、 <elvmx47@aliyun.com>
2019-07-01 09:36:50 +0800

    学生信息修改

--- webserver/src/views/home/student/index.js
+++ webserver/src/views/home/student/index.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { Table, Button, Modal, Form, Input, Radio } from "antd";
-import { connect } from "react-redux";
-import * as actions from "./store/actionCreates";
+import React from 'react';
+import { Table, Button, Modal, Form, Input, Radio } from 'antd';
+import { connect } from 'react-redux';
+import * as actions from './store/actionCreates';
 
 class Student extends React.Component {
   columns = [
     {
-      title: "学号",
-      dataIndex: "_id"
+      title: '学号',
+      dataIndex: '_id'
     },
     {
-      title: "姓名",
-      dataIndex: "studentName"
+      title: '姓名',
+      dataIndex: 'studentName'
     },
     {
-      title: "班级",
-      dataIndex: "gradeId.gradeName"
+      title: '班级',
+      dataIndex: 'gradeId.gradeName'
     },
     {
-      title: "性别",
-      dataIndex: "gender",
+      title: '性别',
+      dataIndex: 'gender',
       render(text, row, index) {
-        return <div>{text ? "男" : "女"}</div>;
+        return <div>{text ? '男' : '女'}</div>;
       }
     },
     {
-      title: "操作",
+      title: '操作',
       render: (text, row, index) => {
         return (
           <div>
@@ -48,36 +48,45 @@ class Student extends React.Component {
    * 显示弹出框的方法
    * @param {Boolean} visible 是否显示
    */
-  showModal = visible => {
+  showModal = () => {
     let curStudentInfo =
       this.props.list.find(item => item._id === this.props.curStudentId) || {};
 
-    console.log(curStudentInfo);
-
-    if (visible) {
+    let UpdateModal = props => {
+      let { getFieldDecorator } = props.form;
       return (
         <Modal
           title="修改信息"
           visible={this.props.visible}
-          onOk={this.props.handleModalOK}
+          onOk={() => {
+            this.props.handleModalOK(props.form);
+          }}
           onCancel={this.props.handleModalCancal}
         >
           <Form labelCol={{ span: 6 }} wrapperCol={{ span: 8 }}>
             <Form.Item label="学生姓名">
-              <Input defaultValue={curStudentInfo.studentName} />
+              {getFieldDecorator('studentName', {
+                rules: [{ required: true, message: '学生姓名不能为空' }],
+                initialValue: curStudentInfo.studentName
+              })(<Input />)}
             </Form.Item>
             <Form.Item label="选择性别">
-              <Radio.Group defaultValue={curStudentInfo.gender}>
-                <Radio value={1}>男</Radio>
-                <Radio value={0}>女</Radio>
-              </Radio.Group>
+              {getFieldDecorator('gender', {
+                initialValue: curStudentInfo.gender
+              })(
+                <Radio.Group>
+                  <Radio value={1}>男</Radio>
+                  <Radio value={0}>女</Radio>
+                </Radio.Group>
+              )}
             </Form.Item>
           </Form>
         </Modal>
       );
-    } else {
-      return null;
-    }
+    };
+
+    UpdateModal = Form.create({})(UpdateModal);
+    return <UpdateModal />;
   };
 
   render() {
@@ -118,8 +127,17 @@ export default connect(
       // 2. 将弹窗给打开
       dispatch(actions.onChgVisible());
     },
-    handleModalOK() {
-      dispatch(actions.onChgVisible());
+    handleModalOK(form) {
+      // 想要得到 form 表单的数据
+      form.validateFields((err, values) => {
+        if (!err) {
+          console.log(values);
+          // 1. 派发动作
+          dispatch(actions.asyncUpdStudent(values));
+          // 2. 关闭弹窗
+          dispatch(actions.onChgVisible());
+        }
+      });
     },
     handleModalCancal() {
       console.log(111);
