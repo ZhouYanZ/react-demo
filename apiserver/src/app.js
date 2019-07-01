431e228e34a12a3563052404111b6b2c12498aa6
庸人自扰、 <elvmx47@aliyun.com>
2019-07-01 08:54:06 +0800

    api支持 put

--- apiserver/src/app.js
+++ apiserver/src/app.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*");
   res.set("Access-Control-Allow-Headers", "content-type");
-  res.set("Access-Control-Allow-Methods", "GET, POST, DELETE");
+  res.set("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
   next();
 });
 
